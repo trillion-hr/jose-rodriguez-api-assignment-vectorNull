7066470b65a9234414919cd0be35b760cb3e985e
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-27 19:13:28 -0800

    Refactor routes

--- routes/ipAddresses.router.js
+++ routes/ipAddresses.router.js
@@ -5,8 +5,8 @@ const ipAddressController = require('../controllers/ipAddresses.controller')
 const ipAddressRouter = express.Router();
 
 ipAddressRouter.get('/list-ip-addresses', ipAddressController.getIpAddresses);
-ipAddressRouter.post('/create-new-ip-addresses/:address', ipAddressController.createIpAddresses);
-ipAddressRouter.post('/aquire-ip-addresses/:address', ipAddressController.aquireIpAddresses);
-ipAddressRouter.post('/release-ip-addresses/:address', ipAddressController.releaseIpAddresses);
+ipAddressRouter.post('/create-new-ip-addresses/', ipAddressController.createIpAddresses);
+ipAddressRouter.post('/aquire-ip-addresses/', ipAddressController.aquireIpAddresses);
+ipAddressRouter.post('/release-ip-addresses/', ipAddressController.releaseIpAddresses);
 
 module.exports = ipAddressRouter;
