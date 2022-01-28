8694a4863fa6ec43ccba9635f718d71067c21e6f
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-27 17:52:00 -0800

    Create ipAddress model/data

---
+++ models/ipAddresses.model.js
@@ -0,0 +1,14 @@
+const ipAddresses = [
+    {
+        id: 0,
+        address: "192.168.1.0",
+        status: "aquired"
+    },
+    {
+        id: 1,
+        address: "192.168.1.2",
+        status: "available"
+    }
+]
+
+module.exports = ipAddresses;
