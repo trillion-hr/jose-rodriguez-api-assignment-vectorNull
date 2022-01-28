884057c0581a10233412db23817ebe56646aab12
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-27 22:17:10 -0800

    Create createIpAddress controller

--- controllers/ipAddresses.controller.js
+++ controllers/ipAddresses.controller.js
@@ -1,24 +1,65 @@
 const IPCIDR = require('ip-cidr');
-const ipAddresses = require("../models/ipAddresses.model")
+const ipAddresses = require("../models/ipAddresses.model");
 
 function getIpAddresses (req, res) {
     res.json(ipAddresses);
 }
 
 function createIpAddresses (req, res) {
+  
     const cidrAddress = req.body.address;
-    if (!cidrAddress) {
+ 
+    if (cidrAddress === '') {
         return res.status(400).json({
-            error: 'Missing CIDR address'
+            error: 'Missing CIDR address',
+            message: 'Please include enter a CIDR address.'
         })
     }
+
+    if(!IPCIDR.isValidAddress(cidrAddress)) {
+        return res.status(400).json({
+            error: 'Invalid CIDR address',
+            message: 'CIDR address is in the wrong format.'
+        })
+    }
+
     
-    
+    const cidr = new IPCIDR(cidrAddress);
+    const addressArray = cidr.toArray();
+
+    // CHECK FOR DUPLICATES
+
+    for (let i = 0; i < addressArray.length; i++) {
+        const result = {};
+        result[i] = {
+            address: addressArray[i],
+            status: "available"
+        }
+        ipAddresses.push(result[i]);
+    }
+    res.send(ipAddresses);
     
 }
 
 function aquireIpAddresses (req, res) {
-    console.log('aquiring ip address');
+    
+    const ipAddress = req.body.address;
+
+
+    for(let i = 0; i < ipAddresses.length; i++) {
+
+        if (ipAddresses[i].address == ipAddress) {
+            ipAddresses[i].status = "aquired";
+            
+            return res.status(200).json({
+                message: `IP address ${ipAddress} has been aquired.`,
+        })
+        } else {
+            return res.status(400).json({
+                message: "IP address not found. Please enter another address."
+            })
+        }      
+    }
 }
 
 function releaseIpAddresses (req, res) {
