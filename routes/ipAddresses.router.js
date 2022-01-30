508198f7ac3ecdb46b7bdd1779643fe4dc3f971d
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-29 17:45:04 -0800

    Deconstruct router functions

--- routes/ipAddresses.router.js
+++ routes/ipAddresses.router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-const ipAddressController = require('../controllers/ipAddresses.controller')
+const {
+    getIpAddresses,
+    createIpAddresses,
+    aquireIpAddresses,
+    releaseIpAddresses
+        } = require('../controllers/ipAddresses.controller')
 
 const ipAddressRouter = express.Router();
 
-ipAddressRouter.get('/list-ip-addresses', ipAddressController.getIpAddresses);
-ipAddressRouter.post('/create-new-ip-addresses/', ipAddressController.createIpAddresses);
-ipAddressRouter.post('/aquire-ip-address/', ipAddressController.aquireIpAddresses);
-ipAddressRouter.post('/release-ip-address/', ipAddressController.releaseIpAddresses);
+ipAddressRouter.get('/list-ip-addresses', getIpAddresses);
+ipAddressRouter.post('/create-new-ip-addresses/', createIpAddresses);
+ipAddressRouter.post('/aquire-ip-address/', aquireIpAddresses);
+ipAddressRouter.post('/release-ip-address/', releaseIpAddresses);
 
 module.exports = ipAddressRouter;
