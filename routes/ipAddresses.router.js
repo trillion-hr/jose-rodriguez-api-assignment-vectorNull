edf1d82c768e909a1166b5929a091ac5b2f7c35e
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-30 16:40:07 -0800

    Update method names

--- routes/ipAddresses.router.js
+++ routes/ipAddresses.router.js
@@ -3,15 +3,15 @@ const express = require('express');
 const {
     getIpAddresses,
     createIpAddresses,
-    aquireIpAddresses,
-    releaseIpAddresses
+    acquireIpAddress,
+    releaseIpAddress,
         } = require('../controllers/ipAddresses.controller')
 
 const ipAddressRouter = express.Router();
 
 ipAddressRouter.get('/list-ip-addresses', getIpAddresses);
 ipAddressRouter.post('/create-new-ip-addresses/', createIpAddresses);
-ipAddressRouter.post('/aquire-ip-address/', aquireIpAddresses);
-ipAddressRouter.post('/release-ip-address/', releaseIpAddresses);
+ipAddressRouter.post('/aquire-ip-address/', acquireIpAddress);
+ipAddressRouter.post('/release-ip-address/', releaseIpAddress);
 
 module.exports = ipAddressRouter;
